d3e03b8f7384c41e20a5e36d419c1caa8d2c0399
Rick Yanashita <ry5@williams.edu>
2023-06-15 20:29:46 -1000

    add linked buttons with icons and uniformity

---
+++ script.js
@@ -0,0 +1,6 @@
+function copyEmail() {
+     // Copy the text inside the text field
+    navigator.clipboard.writeText("ryanashita15@gmail.com");
+    // Alert the copied text
+    alert("email was copied");
+  }
